a1d300918e018d2702d4b6be674b8e2fe6e170e5
Ivan Gonzalez <ivan@simplelogica.net>
2015-11-15 03:37:34 +0100

    Changed Gif controller.

--- app/scripts/controllers/gif.js
+++ app/scripts/controllers/gif.js
@@ -8,10 +8,37 @@
  * Controller of the angularjsGiphyAppApp
  */
 angular.module('angularjsGiphyAppApp')
-  .controller('GifCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('GifCtrl', ['$scope', '$http', '$q', function ($scope, $http, $q) {
+    if($scope.search === undefined){
+      $scope.search = "happy";
+    }
+    if($scope.limit === undefined){
+      $scope.limit = 6;
+    }
+
+    $scope.change = function(){
+      fetch();
+    };
+
+    $scope.update = function(gif){
+      $scope.search = gif.tag;
+      $scope.change();
+    };
+
+    $scope.select = function(){
+      this.setSelectionRange(0, this.value.length);
+    };
+
+    function fetch(){
+      var deferred = $q.defer();
+      $http.get('//api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&' +
+                'q=' + $scope.search +
+                '&limit=' + $scope.limit )
+      .then(function successCallback(response) {
+        deferred.resolve($scope.gifs = angular.fromJson(response.data.data));
+      }, function errorCallback(response) {
+        deferred.resolve($scope.gifs = angular.fromJson({}));
+      });
+      return deferred.promise;
+    };
+  }]);
